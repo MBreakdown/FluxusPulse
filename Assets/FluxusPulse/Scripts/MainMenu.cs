fb00da32671f4ec5c3a5d7eb3658cbf963b3943b
Joza9080 <41316567+Joza9080@users.noreply.github.com>
2018-10-03 15:22:02 +1300

    Menu placeholders completed ~Josh

--- Assets/FluxusPulse/Scripts/MainMenu.cs
+++ Assets/FluxusPulse/Scripts/MainMenu.cs
@@ -4,9 +4,49 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
 
-    public void PlayGame()
+    public void LevelMenu()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void InstructionsMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void HighscoresMenu()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void CreditsMenu()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void QuitMenu()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Menu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
