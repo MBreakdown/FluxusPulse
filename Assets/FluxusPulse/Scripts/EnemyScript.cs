0690ad0716d22546cd6b682c1ebe49aaba59dfb4
Joza9080 <joshua.oc@gmail.com>
2018-10-17 12:28:30 +1300

    Enemy
    The first enemy has been added and is being worked on

---
+++ Assets/FluxusPulse/Scripts/EnemyScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Reference variable, assignable in the inspector.
+    public Transform playerToFollow;
+    public float speed = 10;
+    public float maxSpeed = 20;
+
+	// Use this for initialization
+	void Start()
+    {
+        Debug.Log(playerToFollow.name);
+	}
+
+    // Runs once every physics frame.
+    // Use Time.fixedDeltaTime here
+    // Use Rigidbody stuff here.
+    void FixedUpdate()
+    {
+
+        // Fly towards the player
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (!rb)
+            Debug.LogError("Rigidbody missing!", this);
+
+        Vector2 vectorToPlayer = playerToFollow.transform.position - this.transform.position;
+        rb.AddForce(vectorToPlayer.normalized * speed);
+
+        // Ensure max speed
+        if (rb.velocity.sqrMagnitude > (maxSpeed * maxSpeed))
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+
+        // Rotate towards the correct direction
+        rb.rotation = Mathf.Rad2Deg * Mathf.Atan2(vectorToPlayer.y, vectorToPlayer.x) - 90;
+
+        // Collisions with players
+
+    }
+
+    // Update is called once per frame.
+    // Use Time.deltaTime here.
+    // Use game input here.
+    void Update()
+    {
+
+	}
+}
