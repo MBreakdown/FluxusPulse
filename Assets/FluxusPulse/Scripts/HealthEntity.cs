479f3f23a8ff06bac714d7fca2ac2c054917ae1a
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2018-10-24 14:02:00 +1300

    comments for HealthEntity

--- Assets/FluxusPulse/Scripts/HealthEntity.cs
+++ Assets/FluxusPulse/Scripts/HealthEntity.cs
@@ -5,31 +5,48 @@ using UnityEngine.Events;
 
 public class HealthEntity : MonoBehaviour
 {
-    // Inspector Fields
-
-    [SerializeField]
-    private float m_health = 100f;
-
-    [SerializeField]
-    private float m_maxHealth = 100f;
-
+    // Types
 
     [System.Serializable]
     public class UnityEventFloat : UnityEvent<float> { }
 
+
+    // Inspector Fields
+
+    [Tooltip("Current health, in the range [0..MaxHealth].")]
+    [SerializeField]
+    private float m_health = 100f;
+
+    [Tooltip("Maximum health.")]
+    [SerializeField]
+    private float m_maxHealth = 100f;
+
+    [Tooltip("Event(float Health) invoked after the Health property is changed.")]
     public UnityEventFloat onHealthChanged = new UnityEventFloat();
+
+    [Tooltip("Event(float HealthFraction) invoked after Health property is changed.")]
     public UnityEventFloat onHealthFractionChanged = new UnityEventFloat();
 
+    [Tooltip("Event() invoked after Health property is changed.")]
     public UnityEvent onDeath = new UnityEvent();
 
+    [Tooltip("If this is true when Health reaches 0, this GameObject will be destroyed.")]
     public bool callDestroyOnDeath = false;
+
+    [Tooltip("Delay (seconds) before destruction after Health reaches 0. Requires CallDestroyOnDeath to be true.")]
     public float destroyTime = 0f;
 
     
     // Properties
 
+    /// <summary>
+    /// Maximum health of this entity.
+    /// </summary>
     public float MaxHealth { get { return m_maxHealth; } }
 
+    /// <summary>
+    /// The health of this entity.
+    /// </summary>
     public float Health {
         get { return m_health; }
         set
@@ -44,6 +61,9 @@ public class HealthEntity : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Fraction in the range [0..1], equal to Health / MaxHealth.
+    /// </summary>
     public float HealthFraction {
         get { return Health / MaxHealth; }
         set { Health = value * MaxHealth; }
@@ -52,18 +72,26 @@ public class HealthEntity : MonoBehaviour
 
     // Methods
 
+    /// <summary>
+    /// Decrease Health by an amount.
+    /// </summary>
     public void Hurt(float damage)
     {
         Health -= damage;
     }
 
+    /// <summary>
+    /// Increase Health by an amount.
+    /// </summary>
     public void Heal(float healthIncrease)
     {
         Health += healthIncrease;
     }
 
 
-    void Start()
+    // Unity Events
+    
+    private void Start()
     {
         // Initialise property.
         Health = Health;
