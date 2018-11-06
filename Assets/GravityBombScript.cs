5a42eeba11ec6bedf34cb209141a9244cba077f9
Joza9080 <joshua.oc@gmail.com>
2018-11-06 17:19:08 +1300

    Gravity bomb started being implemented

---
+++ Assets/GravityBombScript.cs
@@ -0,0 +1,56 @@
+﻿/***********************************************************************
+*	Bachelor of Software Engineering
+*	Media Design School
+*	Auckland
+*	New Zealand
+*
+*	(c) 2018 Media Design School
+*
+*	File Name	:	BulletScript.cs
+*	Description	:	Projectile with linear trajectory that can deal damage to player ships.
+*	Project		:	FluxusPulse
+*	Team Name	:	M Breakdown Studios
+***********************************************************************/
+using UnityEngine;
+
+public class GravityBombScript : MonoBehaviour
+{
+    #region Public
+
+    // Inspector Fields
+
+    public float speed;
+    public float damage;
+
+    #endregion Public
+
+    #region Private
+
+    // Unity Event Methods
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = transform.up * speed;
+    }
+    //~ fn
+
+    // Fixed update is called once every physics frame
+    void FixedUpdate()
+    {
+
+    }
+    //~ fn
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //~ fn
+
+    #endregion Private
+}
+//~ class
