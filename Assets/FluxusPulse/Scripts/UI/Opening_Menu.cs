a0373252a3ccd8ee9b2eef2d0a1fafb9ed1c9a08
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2018-11-09 10:32:40 +1300

    OpeningScreen menu controller support (hopefully)

--- Assets/FluxusPulse/Scripts/UI/Opening_Menu.cs
+++ Assets/FluxusPulse/Scripts/UI/Opening_Menu.cs
@@ -1,27 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Opening_Menu : MonoBehaviour {
-
+public class Opening_Menu : MonoBehaviour
+{
 	[SerializeField]
 	private string nextScene;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			SceneManager.LoadScene (nextScene);
-		}
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+    //~ fn
 
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-            QuitUtility.Quit();
-			Debug.Log ("I have quit");
-		}
-	}
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+    //~ fn
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    //~ fn
+
+    public void Quit()
+    {
+        QuitUtility.Quit();
+    }
+    //~ fn
 }
+//~ class
