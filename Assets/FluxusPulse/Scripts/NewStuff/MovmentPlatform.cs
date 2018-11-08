b346b03cf4890d000dbf037cfff79bf0d61b98cf
ArtilectZer0 <41595635+ArtilectZer0@users.noreply.github.com>
2018-11-08 14:30:39 +1300

    Tutorial Work

---
+++ Assets/FluxusPulse/Scripts/NewStuff/MovmentPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentPlatform : MonoBehaviour {
+
+	public bool ismovingleft = false;
+	public float speed = 1;
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Platform") 
+		{
+			ismovingleft = !ismovingleft;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if (ismovingleft) {
+			rb.velocity = new Vector2 (-speed, 0);	
+		}
+		else {
+			rb.velocity = new Vector2 (speed, 0);
+		}
+	}
+}
