446aae2d0f36f36fc4c381964bbcb5f19f1efa55
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2018-11-08 15:42:39 +1300

    fix single spawn

--- Assets/FluxusPulse/Scripts/SingleSpawn.cs
+++ Assets/FluxusPulse/Scripts/SingleSpawn.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingleSpawn : MonoBehaviour {
+public class SingleSpawn : MonoBehaviour
+{
 	[Range(1,2)]
 	public int playerIndexToFollow = 1;
 	public EnemyScript prefab; 
@@ -10,17 +11,14 @@ public class SingleSpawn : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
-		for (int i = 0; i < Count; i++) {
-			var clone = Instantiate<EnemyScript> (prefab, this.transform.position, this.transform.rotation);
+	void Start()
+    {
+		for (int i = 0; i < Count; i++)
+        {
+			EnemyScript clone = Instantiate(prefab, this.transform.position, this.transform.rotation);
 			clone.name = prefab.name;
-			clone.playerToFollow = PlayerShip.GetPlayer (playerIndexToFollow).transform;
-			clone.playerToAvoid = (playerIndexToFollow == 1 ? PlayerShip.GetPlayer2 : PlayerShip.GetPlayer1).transform;
+            clone.playerIndexToFollow = playerIndexToFollow;
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
+//~ class
