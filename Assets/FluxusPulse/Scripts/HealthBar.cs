85f60a5bf63e530c0583e4d591e52e69e3342e9a
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2018-11-08 13:03:04 +1300

    health bars work
    Just make sure the GameController prefab is in the scene.

---
+++ Assets/FluxusPulse/Scripts/HealthBar.cs
@@ -0,0 +1,66 @@
+﻿/***********************************************************************
+*	Bachelor of Software Engineering
+*	Media Design School
+*	Auckland
+*	New Zealand
+*
+*	(c) 2018 Media Design School
+*
+*	File Name	:	HealthEntity.cs
+*	Description	:	Object with health that can be killed.
+*	Project		:	FluxusPulse
+*	Team Name	:	M Breakdown Studios
+*	Author		:	Elijah Shadbolt
+*	Mail		:	elijah.sha7979@mediadesign.school.nz
+***********************************************************************/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+	#region Public
+	
+
+
+	// Properties
+    
+	public float FillFraction {
+		get { return m_image.fillAmount; }
+		set { m_image.fillAmount = value; }
+	}
+	//~ prop
+
+
+
+	// Inspector Fields
+    
+	[SerializeField]
+	private Image m_image;
+
+    [SerializeField]
+    [Range(1, 2)]
+    private int m_playerIndex = 1;
+
+
+
+	#endregion Public
+	#region Private
+
+
+
+	// Unity Events
+
+	void Start()
+	{
+        // Add event listener so this health bar will update automatically.
+        PlayerShip.GetPlayer(m_playerIndex)
+            .GetComponent<HealthEntity>()
+            .onHealthFractionChanged
+            .AddListener((fraction) => FillFraction = fraction);
+	}
+
+
+
+	#endregion Private
+}
+//~ class
